2953d4f737440303f31bc3a09e33244212577c19
AmaraaTs <amarjargal.ts01@gmail.com>
2024-06-19 21:44:09 +0800

    hw ehelsn

---
+++ string_methods.js
@@ -0,0 +1,9 @@
+// bodlogo1. Ugugdsun array ruu too nemj oruuldag function bich. jishee ni: input1: [1, 2, 4] input2: 5 output: [1, 2, 4, 5]
+
+const bod1 = [1, 2, 4];
+
+const nemelt1 = bod1.push(5);
+
+console.log("Bodlogo1: ", bod1);
+
+// bodlogo2. Array uusgeh function bich. jishee ni: input1: [ ] input2: 5 output [1, 2, 3, 4, 5]
