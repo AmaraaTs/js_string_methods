4f1645fa1c76b3944ffb6c0612caf6dc00898750
AmaraaTs <amarjargal.ts01@gmail.com>
2024-06-20 21:47:23 +0800

    finished

--- string_methods.js
+++ string_methods.js
@@ -83,6 +83,126 @@ for (i = 0; i < words.length; i++) {
   reverseWords.push(reverse(words[i]));
 }
 
-console.log(reverseWords);
+console.log("Bodlogo5: ", reverseWords);
 
 // bodlogo6. Ugugdsun text dotor tuhain oruulsan keyword(prompt ashiglah) ni bn uu gedgiin shalgah function bich. text = 'animal world' input: animal output: true, input: cat ouput: false
+
+let search = prompt("Хайх үгээ оруулна уу!");
+
+let searchText = "animal world";
+
+let searchArr = searchText.split(" ");
+
+console.log("Bodlogo6: ", searchArr.includes(search));
+
+// bodlogo7. Дэлгүүрийн боруулалтын мэдэээл өгөгдсөн. productName, unitPrice, amount, price, casherId(Ajiltanii dugaar), date гэсэн мэдээлэл агуулна. Өгөгдлийг өөрсдөө зохионо.
+
+const data = [
+  {
+    productName: "Bakery",
+    unitPrice: 5000,
+    amount: 200,
+    totalPrice: 100000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+
+  {
+    productName: "Chocolate",
+    unitPrice: 3000,
+    amount: 18,
+    totalPrice: 54000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+  {
+    productName: "Funko pop",
+    unitPrice: 50000,
+    amount: 3,
+    totalPrice: 150000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+  {
+    productName: "Watch",
+    unitPrice: 300000,
+    amount: 4,
+    totalPrice: 1200000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+  {
+    productName: "Jordan 1",
+    unitPrice: 500000,
+    amount: 3,
+    totalPrice: 1500000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+  {
+    productName: "Bycicle playing card",
+    unitPrice: 40000,
+    amount: 20,
+    totalPrice: 800000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+  {
+    productName: "Forza horizon 5",
+    unitPrice: 200000,
+    amount: 20,
+    totalPrice: 4000000,
+    casherId: 1,
+    date: "2022-11-01",
+  },
+];
+
+// 7.1. Нийт борлуулалтын дүнг тооцоолох.
+
+function totalSales(x) {
+  let zero = 0;
+  for (i = 0; i < x.length; i++) {
+    zero += x[i].totalPrice;
+  }
+  return zero;
+}
+
+let totalSaless = totalSales(data);
+console.log("Bodlogo7.1: ", totalSaless);
+
+// 7.2. Нийт борлуулагдсан барааны тоог тооцоолох.
+
+function totalQty(x) {
+  let zero = 0;
+  for (i = 0; i < x.length; i++) {
+    zero += x[i].amount;
+  }
+  return zero;
+}
+
+let totalQtys = totalQty(data);
+console.log("Bodlogo7.2: ", totalQtys);
+
+// 7.3. Хамгийн их зарагдсан 5 барааны жагсаалт /боруулалтын үнийн дүнгээр/ харуулах.
+
+let sortSales = data.sort((a, b) => b.totalPrice - a.totalPrice);
+// console.log("Bodlogo7.3: ", sortSales);
+
+function top5sales(x) {
+  for (i = 0; i < 5; i++) {
+    console.log("Bodlogo7.3: ", "top", i + 1, ":", x[i]);
+  }
+}
+let top5saless = top5sales(data);
+// console.log("Bodlogo7.3: ", top5saless);
+
+// 7.4. Хамгийн бага зарагдсан 5 барааны жагсаалт /боруулалтын тоо хэмжээгэр/ харуулах.
+
+let sortqty = data.sort((a, b) => a.amount - b.amount);
+
+function top5qty(x) {
+  for (i = 0; i < 5; i++) {
+    console.log("Bodlogo7.4: ", "bot", i + 1, ":", x[i]);
+  }
+}
+let top5qtys = top5qty(data);
